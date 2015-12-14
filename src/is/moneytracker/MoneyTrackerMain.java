420969aa7656c257ce6ee17ff13c2d61bc1fda10
Van-Duyet Le <me@duyetdev.com>
2015-12-15 02:38:11 +0700

    Update Overview, launch code

--- src/is/moneytracker/MoneyTrackerMain.java
+++ src/is/moneytracker/MoneyTrackerMain.java
@@ -1,14 +1,69 @@
 package is.moneytracker;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
 public class MoneyTrackerMain extends Application {
 
-	@Override
-	public void start(Stage primaryStage) {
+    private Stage primaryStage;
+    private BorderPane rootLayout;
 
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initRootLayout();
+
+        showMoneyTrackerOverview();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MoneyTrackerMain.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showMoneyTrackerOverview() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MoneyTrackerMain.class.getResource("view/Overview.fxml"));
+            AnchorPane dashboardOverview = (AnchorPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(dashboardOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
 
 	public static void main(String[] args) {
 		launch(args);
