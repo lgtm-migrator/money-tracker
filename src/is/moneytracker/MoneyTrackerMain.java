73e23ecf3313e3a1e0664d750e6fb4e496d0f5ce
Van-Duyet Le <me@duyetdev.com>
2015-12-15 02:32:54 +0700

    Initital money tracker with JavaFx, Form

---
+++ src/is/moneytracker/MoneyTrackerMain.java
@@ -0,0 +1,16 @@
+package is.moneytracker;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MoneyTrackerMain extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
